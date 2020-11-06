665281eb88f65b178d6d8de2d9eaf763011595e1
Rudra Desai <rudradesai09@gmail.com>
2020-11-05 21:47:11 -0500

    Added react-ace editor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, {useState, useEffect} from "react";
+import { render } from "react-dom";
+import AceEditor from "react-ace";
 import Socket from './Socket'
 import "./styles.css";
 
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-github";
 
 let socket;
 
 export default function App() {
-    const [name, setName] = useState('')
      useEffect(() => {
-         setName('EZLint')
-
          socket = Socket();
 
          socket.on('test', (data) => {
@@ -21,9 +22,31 @@ export default function App() {
          };
      }, [])
 
+    const onChange = (newValue) => {
+         console.log("change", newValue);
+    }
+
+
     return (
         <div id="body">
-            <h1>{name}</h1>
+            <AceEditor
+                  placeholder=""
+                  mode="javascript"
+                  theme="monokai"
+                  name="blah2"
+                  onChange={onChange}
+                  fontSize={14}
+                  showPrintMargin={true}
+                  showGutter={true}
+                  highlightActiveLine={true}
+                  setOptions={{
+                  enableBasicAutocompletion: false,
+                  enableLiveAutocompletion: false,
+                  enableSnippets: false,
+                  showLineNumbers: true,
+                  tabSize: 2,
+              }}/>
         </div>
     );
 }
+
