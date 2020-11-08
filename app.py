2596a33a7b190e7a630184df14bda314fb3cbf56
weiwei87318 <william87318@gmail.com>
2020-11-08 02:48:50 +0000

    Added database code

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 import os
 import flask
 import flask_socketio
+import flask_sqlalchemy
+import models
 from flask import request
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+from os.path import join, dirname
 
-load_dotenv()
+dotenv_path = join(dirname(__file__), 'sql.env')
+load_dotenv(dotenv_path)
 app = flask.Flask(__name__)
 
 database_url = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db = flask_sqlalchemy.SQLAlchemy(app)
+db.init_app(app)
 db.app = app
+db.create_all()
+db.session.commit()
 
 socketio = flask_socketio.SocketIO(app)
 socketio.init_app(app, cors_allowed_origins="*")
@@ -28,7 +35,6 @@ def on_connect():
     socketio.emit('test', {
         'message': 'Server is up!'
     })
-
 
 if __name__ == '__main__':
     import models
--- models.py
+++ models.py
@@ -1,10 +1,11 @@
 # models.py
 from app import db
+import datetime
 
-class Data(db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(120))
-
+    
     def __init__(self, user_name):
         self.user_name = user_name
 
@@ -12,4 +13,17 @@ class Data(db.Model):
         return str({
             'username': self.user_name,
         })
-
+    
+class History(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    file_name = db.Column(db.String(120))
+    time_stamp = db.Column(datetime)
+    
+    def __init__(self, file_name,time_stamp):
+        self.file_name = file_name
+        self.time_stamp = time_stamp
+    
+    def __repr__(self):
+        return str({
+            'filename': self.file_name, 'time_stamp':self.time_stamp
+        })
