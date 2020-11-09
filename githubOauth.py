61928a109761403af8cfa0e167461e40f35d5200
AnthonyTudorov <anthonytudorov@gmail.com>
2020-11-09 20:36:20 +0000

    now possible to list user repos, repo file structure, and get content from files in repo. DB calls need to be implemented

--- githubOauth.py
+++ githubOauth.py
@@ -1,7 +1,8 @@
 import requests
 import os
 import models
-from flask import session
+import base64
+from flask import request, session
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,9 +18,7 @@ def log_user_info(user_access_token):
     name = user['name']
     email = user['email']
     profile_image = user['avatar_url']
-    #to db => {'login': login, 'name': name, 'email': email, 'profile_image': profile_image, 'access_token': access_token}
-    #from db => get unique id
-    #session['user_id'] = unique id
+    #to db => {'login': login, 'name': name, 'email': email, 'profile_image': profile_image, 'access_token': access_token, 'sid': request.sid}
     
 def auth_user(code, state):
     params = {
@@ -33,36 +32,19 @@ def auth_user(code, state):
         'Accept': 'application/json'
     }
     r = requests.post('https://github.com/login/oauth/access_token', params=params, headers=headers).json()
-    print(r)
     access_token = r['access_token']
-    scopes = r['scope'].split(',')
     
     log_user_info(access_token)
         
 def get_user_data(user_id):
-    #user_info = get user data from db form user_id which corresponds to unique id in db
-    #return user_info
+    #from db using user_id => (login, profile_image)
+    #return {'login': login, 'profile_image': profile_image}
     pass
-
-def get_user_repo_tree(user_id, repo_url):
-    #user_access_token = lookup from db from user_id
-    headers = {
-        'Authorization': 'token ', #+ user_access_token,
-        'Accept': 'application/vnd.github.v3+json'
-    }
-    repo_url = repo_url + '/git/commits/ref'
-    repo = requests.get(repo_url, headers=headers).json()
-    params = {
-        'recursive': True
-    }
-    tree = requests.get(repo['commit']['tree']['url'], params=params)
-    return tree 
-    
 
 def get_user_repos(user_id):
     #user_access_token = lookup from db from user_id
     headers = {
-        'Authorization': 'token ', #+ user_access_token,
+        'Authorization': 'token ' + user_access_token,
         'Accept': 'application/vnd.github.v3+json'
     }
     params = {
@@ -73,4 +55,29 @@ def get_user_repos(user_id):
 
     return [(repo['name'], repo['url']) for repo in repos]
     
+def get_user_repo_tree(user_id, repo_url):
+    #user_access_token = lookup from db from user_id
+    headers = {
+        'Authorization': 'token ' + user_access_token,
+        'Accept': 'application/vnd.github.v3+json'
+    }
+    repo_url = repo_url + '/commits/master'
+    repo = requests.get(repo_url, headers=headers).json()
+    params = {
+        'recursive': True
+    }
+    tree = requests.get(repo['commit']['tree']['url'], params=params, headers=headers).json()
+    return tree['tree']
 
+def get_user_file_contents(user_id, content_url):
+    #user_access_token = lookup from db from user_id
+    headers = {
+        'Authorization': 'token ' + user_access_token,
+        'Accept': 'application/vnd.github.v3+json'
+    }
+    contents = requests.get(content_url, headers=headers).json()
+    if 'content' in contents:
+        return base64.b64decode(contents['content'])
+    else:
+        print('could not determine contents')
+        return None
