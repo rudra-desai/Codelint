9ae5aebd7d52a4097ef1079256c34afb74c6c7a4
AnthonyTudorov <anthonytudorov@gmail.com>
2020-11-11 17:25:39 +0000

    Created necessary database calls

--- githubOauth.py
+++ githubOauth.py
@@ -1,7 +1,8 @@
 import requests
 import os
-import models
 import base64
+import models
+from settings import db
 from flask import request, session
 from dotenv import load_dotenv
 
@@ -18,7 +19,8 @@ def log_user_info(user_access_token):
     name = user['name']
     email = user['email']
     profile_image = user['avatar_url']
-    #to db => {'login': login, 'name': name, 'email': email, 'profile_image': profile_image, 'access_token': access_token, 'sid': request.sid}
+    db.session.add(models.Users(login, name, email, profile_image, user_access_token, request.sid))
+    db.session.commit()
     
 def auth_user(code, state):
     params = {
@@ -37,12 +39,11 @@ def auth_user(code, state):
     log_user_info(access_token)
         
 def get_user_data(user_id):
-    #from db using user_id => (login, profile_image)
-    #return {'login': login, 'profile_image': profile_image}
-    pass
+    query = models.Users.query.filter_by(sid=user_id).first()
+    return {'login': query.login, 'profile_image': query.profile_image}
 
 def get_user_repos(user_id):
-    #user_access_token = lookup from db from user_id
+    user_access_token = models.Users.query.filter_by(sid=user_id).first().access_token
     headers = {
         'Authorization': 'token ' + user_access_token,
         'Accept': 'application/vnd.github.v3+json'
@@ -51,12 +52,15 @@ def get_user_repos(user_id):
         'visibility': 'all'
     }
     repo_url = 'https://api.github.com/user/repos'
-    repos = requests.get(repo_url, params=params, headers=headers).json()
-
-    return [(repo['name'], repo['url']) for repo in repos]
+    repos = requests.get(repo_url, params=params, headers=headers)
+    if repos.status_code == 403:
+        return {'repos': None, 'error': 'bad github token'}
+    
+    repos = repos.json()
+    return {'repos': [(repo['name'], repo['url']) for repo in repos], 'error': None}
     
 def get_user_repo_tree(user_id, repo_url):
-    #user_access_token = lookup from db from user_id
+    user_access_token = models.Users.query.filter_by(sid=user_id).first().access_token
     headers = {
         'Authorization': 'token ' + user_access_token,
         'Accept': 'application/vnd.github.v3+json'
@@ -66,18 +70,25 @@ def get_user_repo_tree(user_id, repo_url):
     params = {
         'recursive': True
     }
-    tree = requests.get(repo['commit']['tree']['url'], params=params, headers=headers).json()
-    return tree['tree']
+    tree = requests.get(repo['commit']['tree']['url'], params=params, headers=headers)
+    if tree.status_code == 403:
+        return {'tree': None, 'error': 'bad github token'}
+    
+    tree = tree.json()
+    return {'tree': tree['tree'], 'error': None}
 
 def get_user_file_contents(user_id, content_url):
-    #user_access_token = lookup from db from user_id
+    user_access_token = models.Users.query.filter_by(sid=user_id).first().access_token
     headers = {
         'Authorization': 'token ' + user_access_token,
         'Accept': 'application/vnd.github.v3+json'
     }
-    contents = requests.get(content_url, headers=headers).json()
-    if 'content' in contents:
-        return base64.b64decode(contents['content'])
+    contents = requests.get(content_url, headers=headers)
+    if contents.status_code == 403:
+        return {'contents': None, 'error': 'bad github token'}
+    
+    contents = contents.json()
+    if 'content' not in contents:
+        return {'contents': None, 'error': 'could not determine contents'}
     else:
-        print('could not determine contents')
-        return None
+        return {'contents': base64.b64decode(contents['content']), 'error': None}
